f41b96c652fde405785ad486fe9d2ed396830130
sunwise <sunwise9634@gmail.com>
2023-07-14 23:50:09 +0900

    leetcode-1218-추가

---
+++ python_src/leet_code/1218-longest-arithmetic-subsequence-of-given-difference.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def longestSubsequence(self, arr, difference):
+        """
+        :type arr: List[int]
+        :type difference: int
+        :rtype: int
+        """
+        result_dict = {}
+        for i in arr:
+            if (i-difference) in result_dict:
+                result_dict[i] = result_dict[i-difference] + 1
+            else:
+                result_dict[i] = 1
+
+        return max(result_dict.values())
+
+if __name__ =="__main__":
+    arr = [1,5,7,8,5,3,4,2,1]
+    print(Solution().maxSubArray(arr, -2))
