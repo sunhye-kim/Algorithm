df331a97129dfc537e91fc912164a1e67895f2dc
sunwise <sunwise9634@gmail.com>
2023-07-14 23:57:20 +0900

    python formatter black 전체 적용

--- python_src/leet_code/1218-longest-arithmetic-subsequence-of-given-difference.py
+++ python_src/leet_code/1218-longest-arithmetic-subsequence-of-given-difference.py
@@ -7,13 +7,14 @@ class Solution(object):
         """
         result_dict = {}
         for i in arr:
-            if (i-difference) in result_dict:
-                result_dict[i] = result_dict[i-difference] + 1
+            if (i - difference) in result_dict:
+                result_dict[i] = result_dict[i - difference] + 1
             else:
                 result_dict[i] = 1
 
         return max(result_dict.values())
 
-if __name__ =="__main__":
-    arr = [1,5,7,8,5,3,4,2,1]
+
+if __name__ == "__main__":
+    arr = [1, 5, 7, 8, 5, 3, 4, 2, 1]
     print(Solution().maxSubArray(arr, -2))
--- python_src/leet_code/53-maximum-subarray.py
+++ python_src/leet_code/53-maximum-subarray.py
@@ -1,14 +1,15 @@
-class Solution():
+class Solution:
     def maxSubArray(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
         for i in range(1, len(nums)):
-            nums[i] = max(nums[i-1]+ nums[i], nums[i])
-        
+            nums[i] = max(nums[i - 1] + nums[i], nums[i])
+
         return max(nums)
 
-if __name__ =="__main__":
-    nums = [-2,1,-3,4,-1,2,1,-5,4]
+
+if __name__ == "__main__":
+    nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
     print(Solution().maxSubArray(nums))
