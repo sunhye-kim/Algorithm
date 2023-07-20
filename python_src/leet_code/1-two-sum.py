08795efb695f5d5d2531a83f61a6f506c5cb0113
sunwise <sunwise9634@gmail.com>
2023-07-20 23:53:32 +0900

    leet code 1 two-sum 수정

--- python_src/leet_code/1-two-sum.py
+++ python_src/leet_code/1-two-sum.py
@@ -5,16 +5,21 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(0, len(nums) - 1):
-            for j in range(i + 1, len(nums)):
-                if (nums[i] + nums[j]) == target:
-                    return [i, j]
+        hash_num = {}
+        for i in range(len(nums)):
+            other_num = target - nums[i]
+            if other_num in hash_num:
+                return [hash_num[other_num], i]
+            hash_num[nums[i]] = i
+
+        return []
+
 
 if __name__ == "__main__":
-    nums = [2,7,11,15]
-    target=9
+    nums = [2, 7, 11, 15]
+    target = 9
     print(Solution().twoSum(nums, target))
 
-    nums = [3,2,3]
-    target=6
+    nums = [3, 2, 3]
+    target = 6
     print(Solution().twoSum(nums, target))
