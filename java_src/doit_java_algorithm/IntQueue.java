2f869fe241df5577a0da1fbe8f9d2fb8daad5f0c
sunhye <sunwise9634@gmail.com>
2025-12-20 07:30:00 +0900

    add IntQueue

---
+++ java_src/doit_java_algorithm/IntQueue.java
@@ -0,0 +1,94 @@
+package doit_java_algorithm;
+
+public class IntQueue {
+    private int max; // 큐 용량
+    private int front; // 첫번쨰 커서
+    private int rear; // 마지막 커서
+    private int num; // 현재 데이터 수
+    private int[] que;
+
+    // 큐가 비어있음
+    public class EmptyIntQueueException extends RuntimeException {
+        public EmptyIntQueueException() {}
+    }
+    // 큐가 가득참
+    public class OverflowIntQueueException extends RuntimeException {
+        public OverflowIntQueueException() {}
+    }
+
+    public IntQueue(int max){
+        num = front = rear = 0;
+        this.max = max;
+        try {
+            que = new int[max];
+        } catch (OutOfMemoryError e) {
+            max = 0;
+        }
+    }
+
+    public int enque(int x) throws OverflowIntQueueException {
+        if (num >= max)
+            throw new OverflowIntQueueException();
+        que[rear++] = x;
+        num++;
+        if (rear == max)
+            rear = 0;
+        return x;
+    }
+
+    public int deque() throws EmptyIntQueueException {
+        if (num <= 0)
+            throw new EmptyIntQueueException();
+        int x = que[front++];
+        num--;
+        if (front == max)
+            front = 0;
+        return x;
+    }
+
+    public int peek() throws EmptyIntQueueException {
+        if (num <= 0)
+            throw new EmptyIntQueueException();
+        return que[front];
+    }
+
+    public int indexOf(int x){
+        for (int i = 0; i < num; i++){
+            int idx = (i + front) % max;
+            if (que[idx] == x)
+                return idx;
+        }
+        return -1;
+    }
+
+    public void clear(){
+        num = front = rear = 0;
+    }
+
+    public int capacity(){
+        return max;
+    }
+
+    public int size(){
+        return num;
+    }
+
+    public boolean isEmpty(){
+        return num <= 0;
+    }
+
+    public boolean isFull(){
+        return num >= max;
+    }
+
+    public void dump(){
+        if (num <= 0)
+            System.out.println("큐가 비어있습니다");
+        else {
+            for (int i = 0; i < num; i++){
+                System.out.println(que[(i + front) % max] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
