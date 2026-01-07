66b0ce8788dbf0f442e8c5d8511b5eb8b9cb54cb
sunhye <sunwise9634@gmail.com>
2026-01-07 23:08:12 +0900

    feat: add recursive EuclidGCD

---
+++ java_src/doit_java_algorithm/recursive/EuclidGCD.java
@@ -0,0 +1,16 @@
+package doit_java_algorithm.recursive;
+
+import java.util.Scanner;
+
+public class EuclidGCD {
+    static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("최대 공약수: "+ gcd(a, b));
+    }
+}
