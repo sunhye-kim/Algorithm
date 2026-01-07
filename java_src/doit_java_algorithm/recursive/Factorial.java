b5cd4a6d5a4f17e6ff768ce673907e46f7397e39
sunhye <sunwise9634@gmail.com>
2026-01-07 22:59:22 +0900

    feat: add recursive factorial

---
+++ java_src/doit_java_algorithm/recursive/Factorial.java
@@ -0,0 +1,18 @@
+package doit_java_algorithm.recursive;
+
+import java.util.Scanner;
+
+public class Factorial {
+    static int factorial(int n) {
+        if (n > 0) return n * factorial(n-1);
+        else return 1;
+    }
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int x = stdIn.nextInt();
+
+        System.out.println(x + "의 팩토리얼은 " + factorial(x) + "입니다.");
+    }
+}
