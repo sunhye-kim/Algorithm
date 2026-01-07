be3f994792d80e7077af6ed7e05b36eafc91b50e
sunhye <sunwise9634@gmail.com>
2026-01-07 23:34:40 +0900

    feat: solve BOJ#17478

---
+++ java_src/boj/recursive/Problem17478.java
@@ -0,0 +1,33 @@
+package boj.recursive;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem17478 {
+    static StringBuilder recursive(int original, int n, StringBuilder sb){
+        String prefix = "____".repeat(original - n);
+        sb.append(prefix).append("\"재귀함수가 뭔가요?\"").append("\n");
+        if (n == 0){
+            sb.append(prefix).append("\"재귀함수는 자기 자신을 호출하는 함수라네\"").append("\n");
+
+        } else {
+            sb.append(prefix).append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.").append("\n");
+            sb.append(prefix).append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.").append("\n");
+            sb.append(prefix).append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"").append("\n");
+            recursive(original, n-1, sb);
+        }
+        sb.append(prefix).append("라고 답변하였지.").append("\n");
+
+        return sb;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.").append("\n");
+        System.out.println(recursive(num, num, sb));
+
+    }
+}
