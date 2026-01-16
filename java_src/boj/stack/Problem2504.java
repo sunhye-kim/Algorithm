cd1cbb522e51a334c78e5779c0356523809e1312
sunhye <sunwise9634@gmail.com>
2026-01-16 22:30:00 +0900

    feat: solve BOJ#2504

--- java_src/boj/stack/Problem2504.java
+++ java_src/boj/stack/Problem2504.java
@@ -3,34 +3,51 @@ package boj.stack;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 import java.util.Stack;
 
 public class Problem2504 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        String input = br.readLine();
 
         Stack<Character> s = new Stack<>();
-        while (true){
-            if (!st.hasMoreTokens()) break;
-
-            char ch = st.nextToken().charAt(0);
-
-            if (ch == '(' || ch == '[') {
+        int result = 0;
+        int tmp = 1;
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (ch == '(') {
                 s.push(ch);
+                tmp *= 2;
+            } else if (ch == '['){
+                s.push(ch);
+                tmp *= 3;
             } else if (ch == ')'){
-                if (s.peek() == '(') {
-                    s.pop();
-                } else  break;
+                if (s.isEmpty() || s.peek() != '(') {
+                    result = 0;
+                    break;
+                }
+                if (input.charAt(i-1) == '(') {
+                    result += tmp;
+                }
+                s.pop();
+                tmp /= 2;
             } else if (ch == ']') {
-                if (s.peek() == '[') {
-                    s.pop();
-                } else  break;
+                if (s.isEmpty() || s.peek() != '[') {
+                    result = 0;
+                    break;
+                }
+                if (input.charAt(i-1) == '[') {
+                    result += tmp;
+                }
+                s.pop();
+                tmp /= 3;
             }
         }
 
-        if (!s.isEmpty()) System.out.println(0);
+        if (!s.isEmpty()) {
+            result = 0;
+        }
+        System.out.println(result);
 
     }
 }
